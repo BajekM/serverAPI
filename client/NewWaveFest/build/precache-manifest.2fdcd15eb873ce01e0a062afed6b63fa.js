0a6602c72151bc58a6fd6511e3de742cc277023c
Marcin Bajek <bajekmarcin@gmail.com>
2020-02-29 05:05:02 +0100

    Delete /build from .gitignore

---
+++ client/NewWaveFest/build/precache-manifest.2fdcd15eb873ce01e0a062afed6b63fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae681a8fd00d8f494f823be17f915199",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b825cca8d8091e1c6d61",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fae5aa1b1dfb1d044156",
+    "url": "/static/css/main.8c976a40.chunk.css"
+  },
+  {
+    "revision": "b825cca8d8091e1c6d61",
+    "url": "/static/js/2.258950aa.chunk.js"
+  },
+  {
+    "revision": "fae5aa1b1dfb1d044156",
+    "url": "/static/js/main.dcc249c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
